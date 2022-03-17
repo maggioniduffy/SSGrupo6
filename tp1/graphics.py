248f3234c7e9c5d7609c69bdb30575c4c04a4c65
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-03-17 14:20:41 -0300

    hardcoded neighbors highlighted added

--- tp1/graphics.py
+++ tp1/graphics.py
@@ -4,43 +4,48 @@ import matplotlib.pyplot as plt
 # Fixing random state for reproducibility
 # np.random.seed(19680801)
 f = open('Dynamic100.txt','r')
+g = open('AlgunosVecinos_100_rc6.txt', 'r')
 
 lines = f.readlines() # read all the lines of the text file and return them as a list of strings.
 f.close()
 lines.pop(0)
+
+g_lines = g.readlines() # read all the lines of the text file and return them as a list of strings.
+g.close()
+
 x_arr = []
 y_arr = []
 
-for line in lines:
-    print(line)
+colors = []
+area = []
+
+neighbors = []
+
+for i in range(len(lines)):
+    line = lines[i]
     numbers = line.strip().split(' ')
-    print(numbers)
+
     x_arr.append(float(numbers[0]))
     y_arr.append(float(numbers[3]))
+
+    if (i == 91):
+        colors.append('#FF0000')
+        area.append(60)
+        neighbors = (g_lines[i-1].strip().split(','))[1:]
+        print(neighbors)
+    else:
+        colors.append('#000')
+        area.append(35)
+
+for neigh in neighbors:
+    index = int(neigh)
+    colors[index] = '#FFFA10'
+    area[index] = 55
 
 x = np.array(x_arr)
 y = np.array(y_arr)
 
-g = open('AlgunosVecinos_100_rc6.txt', 'r')
-g_lines = g.readlines() # read all the lines of the text file and return them as a list of strings.
-g.close()
-
-neighbors = []
-
-for line in g_lines:
-    numbers = line.strip().split(',')
-    neighbors.append(numbers)
-
 N = len(x)
-colors = []
-area = []
-for i in range(N):
-    if (i == 91):
-        colors.append('#FF0000')
-        area.append(60)
-    else:
-        colors.append('#000')
-        area.append(35)
 
 plt.grid()
 plt.scatter(x, y, s=area, linewidths=1, c=colors, alpha=0.8)
