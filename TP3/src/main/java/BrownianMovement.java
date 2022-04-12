8d0076debc56437fb37427f6b44b6992bd3045b8
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-04-12 15:02:18 -0300

    metodos agregados, chequear

--- TP3/src/main/java/BrownianMovement.java
+++ TP3/src/main/java/BrownianMovement.java
@@ -19,14 +19,13 @@ public class BrownianMovement {
     }
 
     public void start(){
-        List<Particle> copy = (List<Particle>) this.particles.clone();
-        states.put(time, copy);
+        List<Particle> clone = (List<Particle>) this.particles.clone();
+        states.put(time, clone);
 
         while(!finish()) {
             float next = getNextCollisionTime();
             updateStatus(next);
-            generateCollision();
-
+            updateSpeeds();
             iterations +=1;
         }
     }
@@ -53,15 +52,15 @@ public class BrownianMovement {
 
         for (int i = 0; i < particles.size(); i++) {
 
-            float verticalCollisionTime = particles.get(i).verticalWallCollision(board.SIDE_SIZE);
-            float horizontalCollisionTime = particles.get(i).horizontalWallCollision(board.SIDE_SIZE));
+            float verticalCollisionTime = particles.get(i).timeToVerticalWallCollision(board.SIDE_SIZE);
+            float horizontalCollisionTime = particles.get(i).timeToHorizontalWallCollision(board.SIDE_SIZE);
             float particleCollisionTime = Float.MAX_VALUE;
 
             int selectedJ = 0;
 
             for (int j = i + 1; j < particles.size(); j++) {
 
-                float particleTime = particles.get(i).particleCollision(particles.get(j));
+                float particleTime = particles.get(i).timeToParticleCollision(particles.get(j));
 
                 if (particleTime < particleCollisionTime) {
                     particleCollisionTime = particleTime;
@@ -94,5 +93,55 @@ public class BrownianMovement {
         return nextEventTime;
     }
 
+    private void updateStatus(float collisionTime) {
+        for (Particle p : particles) {
+            p.updatePosition(collisionTime);
+        }
+        time += collisionTime;
+        List<Particle> clone = (List<Particle>) this.particles.clone();;
+        states.put(time, clone);
+    }
 
+    private void updateSpeeds(){
+        for (Collision c : colls){
+            if (c.type == CollisionType.VERTICALWALL) {
+                double xVel = c.p1.getVelX() * -1;
+                c.p1.setVelX(xVel);
+            } else if (c.type == CollisionType.HORIZONTALWALL) {
+                double yVel = c.p1.getVelY() * -1;
+                c.p1.setVelY(yVel);
+            } else {
+
+                double deltaX = c.p1.getPosX() - c.p2.getPosX();
+                double deltaY = c.p1.getPosY() - c.p2.getPosY();
+                double deltaR[] = {deltaX, deltaY};
+
+                double deltaVX = c.p1.getVelX() - c.p2.getVelX();
+                double deltaVY = c.p1.getVelY() - c.p2.getVelY();
+                double deltaV[] = {deltaVX, deltaVY};
+                double esc = deltaV[0] * deltaR[0] + deltaV[1] * deltaR[1]; //deltaV.deltaR
+
+                double sigma = c.p1.getRad() + c.p2.getRad();
+
+                double m1 = c.p1.getMass();
+                double m2 = c.p2.getMass();
+
+                double j = (2 * m1 * m2 * esc) / (sigma * (m1 + m2));
+                double jX = (j * (c.p1.getPosX() - c.p2.getPosX())) / sigma;
+                double jY = (j * (c.p1.getPosY() - c.p2.getPosY())) / sigma;
+
+                double velP1x = c.p1.getVelX() - (jX / m1);
+                double velP1y = c.p1.getVelY() - (jY / m1);
+
+                c.p1.setVelX(velP1x);
+                c.p1.setVelY(velP1y);
+
+                double velP2x = c.p2.getVelX() + (jX / m2);
+                double velP2y = c.p2.getVelY() + (jY / m2);
+
+                c.p2.setVelX(velP2x);
+                c.p2.setVelY(velP2y);
+            }
+        }
+    }
 }
--- TP3/src/main/java/Particle.java
+++ TP3/src/main/java/Particle.java
@@ -14,7 +14,7 @@ public class Particle {
         return posX;
     }
 
-    public void setPosX(float posX) {
+    public void setPosX(double posX) {
         this.posX = posX;
     }
 
@@ -22,7 +22,7 @@ public class Particle {
         return posY;
     }
 
-    public void setPosY(float posY) {
+    public void setPosY(double posY) {
         this.posY = posY;
     }
 
@@ -30,7 +30,7 @@ public class Particle {
         return velX;
     }
 
-    public void setVelX(float velX) {
+    public void setVelX(double velX) {
         this.velX = velX;
     }
 
@@ -38,7 +38,7 @@ public class Particle {
         return velY;
     }
 
-    public void setVelY(float velY) {
+    public void setVelY(double velY) {
         this.velY = velY;
     }
 
@@ -46,7 +46,7 @@ public class Particle {
         return rad;
     }
 
-    public void setRad(float rad) {
+    public void setRad(double rad) {
         this.rad = rad;
     }
 
@@ -54,7 +54,7 @@ public class Particle {
         return mass;
     }
 
-    public void setMass(float mass) {
+    public void setMass(double mass) {
         this.mass = mass;
     }
 
@@ -62,7 +62,61 @@ public class Particle {
         return collTime;
     }
 
-    public void setCollTime(float collTime) {
+    public void setCollTime(double collTime) {
         this.collTime = collTime;
+    }
+
+    public void updatePosition(float time) {
+        this.posX = this.posX + (this.velX * time); //Habria que usar la velocidad inicial en vez de this.posX en el primer termino?
+        this.posY = this.posY + (this.velY * time); //Igual que con X
+    }
+
+    public float timeToParticleCollision(Particle particle){
+
+        double deltaR[] = {this.posX - particle.getPosX(), this.posY - particle.getPosY()};
+        double deltaV[] = {this.velX - particle.getVelX(), this.velY - particle.getVelY()};
+        double deltas = deltaV[0] * deltaR[0] + deltaV[1] * deltaR[1];
+        if (deltas >= 0){
+            return Float.MAX_VALUE;
+        }
+        double sigma = this.rad + particle.getRad();
+        float escDeltaV = (float) (Math.pow(deltaV[0], 2) + Math.pow(deltaV[1], 2));
+        float escDeltaR = (float) (Math.pow(deltaR[0], 2) + Math.pow(deltaR[1], 2));
+
+        float d = (float) (Math.pow(deltas, 2) - escDeltaV * ( escDeltaR - Math.pow(sigma, 2)));
+        if (d < 0){
+            return Float.MAX_VALUE;
+        }
+        float collisionTime = (float) -((deltas + Math.sqrt(d)) / escDeltaV);
+
+        return  collisionTime;
+    }
+
+    public float timeToVerticalWallCollision(float size){
+        float collisionTime;
+        float collisionTimeXver = Float.MAX_VALUE;
+
+        if (this.velX > 0.0f){
+            collisionTimeXver =  (float) ((size - this.rad - this.posX) / this.velX);
+        } else if (this.velX < 0.0f) {
+            collisionTimeXver = (float) ((this.rad - this.posX) / this.velX);
+        }
+
+        collisionTime = collisionTimeXver ;
+        return collisionTime;
+    }
+
+    public Float timeToHorizontalWallCollision(float size){
+        Float collisionTime;
+        Float collisionTimeYhor=Float.MAX_VALUE;
+
+        if (this.velY > 0.0f){
+            collisionTimeYhor =  (float) ((size - this.rad - this.posY) / this.velY);
+        } else if (this.velY < 0.0f){
+            collisionTimeYhor = (float) ((this.rad - this.posY) / this.velY);
+        }
+        collisionTime = collisionTimeYhor;
+
+        return collisionTime;
     }
 }
